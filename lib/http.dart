ee2e65be0b678f61a56f98e47fb023216df7ac08
ting <ticccccc@163.com>
2018-11-25 15:23:45 +0800

    第四步，解析并显示，在解析中出现错误

--- lib/http.dart
+++ lib/http.dart
@@ -8,72 +8,77 @@ class NetUtil {
   ///get请求
   static void get(String url, Function callBack,
       {Map<String, String> params, Function errorCallBack}) async {
-
+    _request(url, callBack,
+        method: GET, params: params, errorCallback: errorCallBack);
   }
 
+  ///post请求
+  static void post(String url, Function callBack,
+      {Map<String, String> params, Function errorCallback}) async {
+    _request(url, callBack,
+        method: POST, params: params, errorCallback: errorCallback);
+  }
 
   ///具体的还要看返回数据，公共代码部分
-  static void _request(String url,Function callBack,
-  {String method,
-  Map<String,String> params,
-  Function errorCallback}) async{
-    print("<net> url :<"+method+">"+url);
+  static void _request(String url, Function callBack,
+      {String method,
+      Map<String, String> params,
+      Function errorCallback}) async {
+    print("<net> url :<" + method + ">" + url);
 
-    if(params!=null&&params.isNotEmpty){
-      print("<net> url params :"+params.toString());
+    if (params != null && params.isNotEmpty) {
+      print("<net> url params :" + params.toString());
     }
 
     //这里要根据项目自适应
-    String errorMsg ="";
+    String errorMsg = "";
     int statusCode;
-    try{
+    try {
       Response response;
-      if(method==GET){
+      if (method == GET) {
         //有参数的GET请求
-        if(params!=null&&params.isNotEmpty){
+        if (params != null && params.isNotEmpty) {
           StringBuffer sb = new StringBuffer("?");
-          params.forEach((key,value){
-            sb.write("$key"+"="+"$value"+"&");
+          params.forEach((key, value) {
+            sb.write("$key" + "=" + "$value" + "&");
           });
-          String paramStr =sb.toString();
-          paramStr = paramStr.substring(0,paramStr.length-1);
-          url+=paramStr;
+          String paramStr = sb.toString();
+          paramStr = paramStr.substring(0, paramStr.length - 1);
+          url += paramStr;
         }
         //没有参数，有参数
         response = await Dio().get(url);
-      }else{
+      } else {
         //请求
-        if(params!=null&&params.isNotEmpty){
-          response = await Dio().post(url,data: params);
-        }else{
+        if (params != null && params.isNotEmpty) {
+          response = await Dio().post(url, data: params);
+        } else {
           response = await Dio().post(url);
         }
       }
-      statusCode =response.statusCode;
+      statusCode = response.statusCode;
 
       //处理错误部分
-      if(statusCode<0){
-        errorMsg ="网络请求错误，状态码："+statusCode.toString();
+      if (statusCode < 0) {
+        errorMsg = "网络请求错误，状态码：" + statusCode.toString();
         _handError(errorCallback, errorMsg);
         return;
       }
 
-      if(callBack!=null){
+      if (callBack != null) {
+        print("<response> :"+response.data["data"].toString());
         callBack(response.data["data"]);
       }
-
-    }catch(exception){
+    } catch (exception) {
       _handError(errorCallback, exception.toString());
     }
-
   }
 
   //处理异常
-static void _handError(Function errorCallback,String errorMsg){
-    if(errorCallback!=null){
+  static void _handError(Function errorCallback, String errorMsg) {
+    if (errorCallback != null) {
       errorCallback(errorMsg);
     }
-    print("<net> errorMsg : "+errorMsg);
+    print("<net> errorMsg : " + errorMsg);
   }
-
 }
---
+++ lib/http_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'http.dart';
+import 'user.dart';
+class JsonSeralizablePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _JsonSeralizablePageState();
+  }
+}
+
+class _JsonSeralizablePageState extends State<JsonSeralizablePage> {
+  String name = "";
+  String email = "";
+  List<String> picList = <String>[];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JSON"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Text(
+              name,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              email,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            SizedBox(
+              height: 250.0 * picList.length,
+              child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                return picList != null && picList.length > 0
+                    ? Image(
+                        image: NetworkImage(picList[index]),
+                        width: 400.0,
+                        height: 250.0,
+                        fit: BoxFit.cover,
+                      )
+                    : Text("no pic");
+              }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getContent();
+    
+  }
+
+  void getContent() {
+    NetUtil.get("http://www.mocky.io/v2/5bfa43493200007300f2261d",(data){
+      User user = User.fromJson(data);
+      print("<user> : "+ user.toString());
+      setState(() {
+        name =user.data.name;
+        email =user.data.email;
+        picList =user.data.pics;
+      });
+    },errorCallBack: (errorMsg){
+      print("error:"+errorMsg);
+    });
+  }
+}
