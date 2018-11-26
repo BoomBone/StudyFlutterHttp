f4452042c61e93e4a84785279f58af705691aff6
Ting <ticccccc@163.com>
2018-11-26 16:12:18 +0800

    成功完成json解析并显示

--- lib/http_page.dart
+++ lib/http_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'http.dart';
 import 'user.dart';
+
 class JsonSeralizablePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -47,16 +48,20 @@ class _JsonSeralizablePageState extends State<JsonSeralizablePage> {
             SizedBox(
               height: 250.0 * picList.length,
               child: ListView.builder(
+                  physics: ClampingScrollPhysics(),
+                  itemCount: picList != null && picList.length > 0
+                      ? picList.length
+                      : 0,
                   itemBuilder: (BuildContext context, int index) {
-                return picList != null && picList.length > 0
-                    ? Image(
-                        image: NetworkImage(picList[index]),
-                        width: 400.0,
-                        height: 250.0,
-                        fit: BoxFit.cover,
-                      )
-                    : Text("no pic");
-              }),
+                    return picList != null && picList.length > 0
+                        ? Image(
+                            image: NetworkImage(picList[index]),
+                            width: 400.0,
+                            height: 250.0,
+                            fit: BoxFit.cover,
+                          )
+                        : Text("no pic");
+                  }),
             )
           ],
         ),
@@ -69,20 +74,20 @@ class _JsonSeralizablePageState extends State<JsonSeralizablePage> {
     // TODO: implement initState
     super.initState();
     getContent();
-    
   }
 
   void getContent() {
-    NetUtil.get("http://www.mocky.io/v2/5bfa43493200007300f2261d",(data){
+    NetUtil.get("http://www.mocky.io/v2/5bfa43493200007300f2261d", (data) {
+      print("<data> : " + data.toString());
       User user = User.fromJson(data);
-      print("<user> : "+ user.toString());
+      print("<user> : " + user.toString());
       setState(() {
-        name =user.data.name;
-        email =user.data.email;
-        picList =user.data.pics;
+        name = user.name;
+        email = user.email;
+        picList = user.pics;
       });
-    },errorCallBack: (errorMsg){
-      print("error:"+errorMsg);
+    }, errorCallBack: (errorMsg) {
+      print("error:" + errorMsg);
     });
   }
 }
--- lib/user.dart
+++ lib/user.dart
@@ -1,44 +1,46 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'dart:convert' show json;
 
-part 'user.g.dart';
+class Users {
 
-
-@JsonSerializable()
-class User extends Object {
-
-  @JsonKey(name: 'statusCode')
   int statusCode;
+  User data;
 
-  @JsonKey(name: 'data')
-  Data data;
+  Users.fromParams({this.statusCode, this.data});
 
-  User(this.statusCode,this.data,);
+  factory Users(jsonStr) => jsonStr == null ? null : jsonStr is String ? new Users.fromJson(json.decode(jsonStr)) : new Users.fromJson(jsonStr);
 
-  factory User.fromJson(Map<String, dynamic> srcJson) => _$UserFromJson(srcJson);
+  Users.fromJson(jsonRes) {
+    statusCode = jsonRes['statusCode'];
+    data = jsonRes['data'] == null ? null : new User.fromJson(jsonRes['data']);
+  }
 
-  Map<String, dynamic> toJson() => _$UserToJson(this);
-
+  @override
+  String toString() {
+    return '{"statusCode": $statusCode,"data": $data}';
+  }
 }
 
+class User {
 
-@JsonSerializable()
-class Data extends Object {
-
-  @JsonKey(name: 'name')
-  String name;
-
-  @JsonKey(name: 'email')
   String email;
-
-  @JsonKey(name: 'pics')
+  String name;
   List<String> pics;
 
-  Data(this.name,this.email,this.pics,);
+  User.fromParams({this.email, this.name, this.pics});
 
-  factory Data.fromJson(Map<String, dynamic> srcJson) => _$DataFromJson(srcJson);
+  User.fromJson(jsonRes) {
+    email = jsonRes['email'];
+    name = jsonRes['name'];
+    pics = jsonRes['pics'] == null ? null : [];
 
-  Map<String, dynamic> toJson() => _$DataToJson(this);
+    for (var picsItem in pics == null ? [] : jsonRes['pics']){
+      pics.add(picsItem);
+    }
+  }
 
+  @override
+  String toString() {
+    return '{"email": ${email != null?'${json.encode(email)}':'null'},"name": ${name != null?'${json.encode(name)}':'null'},"pics": $pics}';
+  }
 }
-
 
